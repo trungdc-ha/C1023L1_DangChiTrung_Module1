fec1ec736222b159ca1b9f3734ddb37ecfd97ee5
trungdc-ha <dangchitrungha@gmail.com>
2023-11-13 21:03:14 +0700

    TrungDC commit demo bài tổng quan Javascript

---
+++ ss06_javascript/myJS.js
@@ -0,0 +1,12 @@
+function updateTitle() {
+    alert("ok")
+    document.getElementById("txtTitle").innerText = "C1023"
+}
+function updateTitle2() {
+    alert("ok")
+    document.getElementById("txtTitle").innerText = "C1023"
+}
+function updateTitle1() {
+    alert("ok")
+    document.getElementById("txtTitle").innerText = "C1023"
+}
